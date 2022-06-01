f6505603c3b679bb023573e9453126e8c7e7c305
nimrodds <nsartral@student.42.fr>
2022-06-01 06:59:01 +0200

    in progretsion

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -23,9 +23,74 @@ char *find_path(char *path, char *arg)
 	return (NULL);
 }
 
+char **parse_arguments(char *str, char *target)
+{
+	char **tmp;
+	char **argz;
+	int i;
+	int j;
+	
+	tmp = ft_split(str, ' ');
+	// i = 0;
+	// while (tmp[i])
+	// {
+	// 	ft_printf("%s\n", tmp[i]);
+	// 	i++;
+	// }
+	i = 0;
+	while (tmp[i])
+		i++;
+	argz = (char **)malloc(sizeof(char *) * (i + 3));
+	if (argz == NULL)
+		return (NULL);
+	argz[0] = "wejdene";
+	argz[i + 2] = NULL;
+	j = 1;
+	while (j <= i)
+	{
+		argz[j] = tmp[j - 1];
+		j++;
+	}
+	argz[j] = target;
+	// i = 0;
+	// while (argz[i])
+	// {
+	// 	ft_printf("%s\n", argz[i]);
+	// 	i++;
+	// }
+	return (argz);
+}
+
+char *trim_command(char *str)
+{
+	int i;
+	int j;
+	char *command;
+	
+	i = 0;
+	while (str[i] && str[i] == ' ' && str[i] == '\t' && str[i] == '\v' && str[i] == '\n' && str[i] == '\r' && str[i] == '\f')
+		i++;
+	j = 0;
+	while (str[i + j] >= 'a' && str[i + j] <= 'z')
+		j++;
+	if (j == 0)
+		return (NULL);
+	command = (char *)malloc(sizeof(char) * (j + 1));
+	if (command == NULL)
+		return (NULL);
+	command[j] = '\0';
+	j = 0;
+	while (str[i + j] >= 'a' && str[i + j] <= 'z')
+	{
+		command[j] = str[i + j];
+		j++;
+	}
+	return (command);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
-	char *argz1[4];
+	char **argz1;
 	char *argz2[3];
 	int count;
 	char buffer[4096];
@@ -46,6 +111,7 @@ int	main(int argc, char **argv, char **envp)
 	
 	int i;
 	char *path;
+	char *command;
 	char **paths;
 	
 	i = 0;
@@ -57,20 +123,24 @@ int	main(int argc, char **argv, char **envp)
 		path++;
 	paths = ft_split(path, ':');
 	i = 0;
-	while (paths[i] && !find_path(paths[i], argv[2]))
+	command = trim_command(argv[2]);
+	while (paths[i] && !find_path(paths[i], command))
 		i++;
 	if (paths[i])
-		path = find_path(paths[i], argv[2]);
+		path = find_path(paths[i], command);
 	else
 		return (ft_printf("display error"), 0);
 	// ft_printf("this is the path %s\n", path);
 	
 	if (pipe(fd) == -1)
 		return (0);
+
+	argz1 = parse_arguments(argv[2], argv[1]);
 	
-	argz1[0] = path;
-	argz1[1] = argv[1];
-	argz1[2] = NULL;
+	// argz1[0] = path;
+	// argz1[1] = "-e";
+	// argz1[2] = argv[1];
+	// argz1[3] = NULL;
 	
 	pid1 = fork();
 	if (pid1 == -1)
@@ -83,9 +153,9 @@ int	main(int argc, char **argv, char **envp)
 		execve(path, argz1, envp);
 	}
 	
-		// count = read(fd[0], buffer, 4096);
-		// buffer[count] = '\0';
-		// ft_printf("%s", buffer);
+	// 	// count = read(fd[0], buffer, 4096);
+	// 	// buffer[count] = '\0';
+	// 	// ft_printf("%s", buffer);
 	
 	i = 0;
 	while (paths[i] && !find_path(paths[i], argv[3]))
